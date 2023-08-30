0b76b6fe4e4bcfa5c06476afea1b54ec7b703fd5
Rosana Rezende <rezende_rosana@hotmail.com>
2023-08-29 21:28:51 -0300

    adiciona rotas e páginas

---
+++ src/pages/CadastroDeEscola/CadastroDeEscola.jsx
@@ -0,0 +1,5 @@
+export default function CadastroDeEscola() {
+  return (
+    <div>CadastroDeEscola</div>
+  )
+}
---
+++ src/pages/Inicio/Inicio.jsx
@@ -0,0 +1,10 @@
+import Menu from "../../components/Menu/Menu";
+
+export default function Inicio() {
+  return (
+    <>
+      <Menu />
+      <div>Inicio</div>
+    </>
+  );
+}
